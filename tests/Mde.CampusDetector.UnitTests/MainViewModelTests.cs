bd3ef14b04068b075d0805e8b27ce990463b1060
sigged <sderdeyn@gmail.com>
2024-03-24 17:39:16 +0100

    Add unit test project

---
+++ tests/Mde.CampusDetector.UnitTests/MainViewModelTests.cs
@@ -0,0 +1,11 @@
+namespace Mde.CampusDetector.UnitTests
+{
+    public class MainViewModelTests
+    {
+        [Fact]
+        public void IsSane()
+        {
+            Assert.True(true);
+        }
+    }
+}
