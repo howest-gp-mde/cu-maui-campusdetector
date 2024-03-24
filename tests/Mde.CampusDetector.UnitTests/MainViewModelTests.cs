9253e3f8fd57e46c9bdeca59d4349c6c71bc2e4a
sigged <sderdeyn@gmail.com>
2024-03-24 18:15:11 +0100

    Remove IsSane test

--- tests/Mde.CampusDetector.UnitTests/MainViewModelTests.cs
+++ tests/Mde.CampusDetector.UnitTests/MainViewModelTests.cs
@@ -2,10 +2,6 @@ namespace Mde.CampusDetector.UnitTests
 {
     public class MainViewModelTests
     {
-        [Fact]
-        public void IsSane()
-        {
-            Assert.True(true);
-        }
+
     }
 }
